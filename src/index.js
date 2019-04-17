c924156a40cf386c101b720c04b78fee16d14be6
Godsgift Akari <giftakari@yahoo.com>
2019-04-18 09:31:06 +1000

    forms and Inline styling

--- src/index.js
+++ src/index.js
@@ -6,9 +6,15 @@ import {render} from 'react-dom';
 const App =() => {
   let a = 34;
   let b = 70;
-  let sum = a + b;
+  let sum = a * b;
   return <div>
-    Hi there, Thanks for and welcome{a} +{b} = {sum}
+    <h1 style={{backgroundColor:'gold',color:'#fff',border:'5px solid red'}}>Hi there, Thanks for and welcome {a} * {b} = {sum}</h1>
+    <label className="label" for="name">
+      Enter Name: 
+    </label>
+    <input id="name" type="text" />
+    <button style={{background:'gold', color:'#fff', borderRadius:'1em 2em'}}>Submit</button>
+
   </div>
 }
 
